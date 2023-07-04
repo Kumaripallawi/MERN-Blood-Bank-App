58d6b9681a9d78b3d0a6b3f37487bc8fcb31a8e1
Pallawi <pallawikumaribth80@gmail.com>
2023-07-04 11:30:19 +0530

    Added my project

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan =require("morgan");
+const cors = require("cors")
+
+
+//dot config
+dotenv.config()
+
+//rest object
+const app = express();
+
+//Middleware
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+ //routes
+ app.use("/api/v1/test",require("./routes/testRoutes"));
+
+ //port
+ const PORT=process.env.PORT || 8080;
+
+ //listen
+ app.listen(PORT,()=>{
+    console.log(`Node Server is Running In ${process.env.DEV_MODE} Mode On Port ${process.env.PORT}`
+    .bgBlue.white);
+ });
